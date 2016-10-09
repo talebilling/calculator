d67a2ff8d70f3c4055b95b69f72ef30cc0774bec
Emese Billing <talebilling@gmail.com>
2016-10-09 16:28:19 +0200

    calculator refactor

--- calculator.py
+++ calculator.py
@@ -3,27 +3,30 @@ import sys
 import operator
 import re
 
-while (True):
-    pattern = re.compile("[0-9]+")
+while True:
+    number_pattern = re.compile("^-?[0-9]+$")
 
     first_input = str(input('Enter a number (or a letter to exit): '))
-    if pattern.match(first_input):
+    if number_pattern.match(first_input):
         first_number = int(first_input)
-
-        ops = {"+": operator.add, "-": operator.sub,"*": operator.mul,"/": operator.truediv}
-        sign = input('Enter an operation: ')
-        if sign in ops:
-            op_func = ops[sign]
-        else:
-            break
-
-        second_input = str(input('Enter another number: '))
-        if pattern.match(second_input):
-            second_number = int(second_input)
-        else:
-            break
-
-        result = round(op_func(first_number, second_number))
-        print('Result:', result)
     else: 
         break
+        
+    op_dictionary = {"+": operator.add, 
+                    "-": operator.sub,
+                    "*": operator.mul,
+                    "/": operator.truediv}
+    op_sign = input('Enter an operation: ')
+    if op_sign in op_dictionary:
+        operation = op_dictionary[op_sign]
+    else:
+        break
+
+    second_input = str(input('Enter another number: '))
+    if number_pattern.match(second_input):
+        second_number = int(second_input)
+    else:
+        break
+
+    result = round(operation(first_number, second_number))
+    print('Result:', result, '\n')
